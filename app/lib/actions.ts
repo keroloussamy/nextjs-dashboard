cc616fde08298b97d053c52173459a546aea0112
keroloussamy <keroloussamy98@gmail.com>
2023-12-18 01:00:34 +0200

    Validate using Zod, inserting into DB, revalidate and redirect.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,38 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), // Set to coerce (change) from a string to a number while also validating its type.
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// Server Action
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // const rawFormData2 = Object.fromEntries(formData.entries())
-  console.log(rawFormData);
+  });
+  // const rawFormData = Object.fromEntries(formData.entries())
+  const amountInCents = amount * 100; // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const date = new Date().toISOString().split('T')[0]; // "YYYY-MM-DD"
+
+  // Inserting the data into database.
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Revalidate and redirect.
+  revalidatePath('/dashboard/invoices'); // To clear the cached data and trigger a new request to the server.
+  redirect('/dashboard/invoices');
 }
